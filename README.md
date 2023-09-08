30029fc24c500d11d303eee52d1c5c280b212ae9
Caio Cavalcanti <caio.marques.cavalcanti@rtl.nl>
2023-09-08 15:53:17 +0200

    Add high level architecture diagram

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# azure-cost-reporting
+# Azure Cost Reporting
+
 A web app to help you monitor your Azure cost.
+
+# Requirements
+
+- Python 3.11
+
+# Running locally
+
+```sh
+# Create your virtual environment
+python -m venv .venv
+source .venv/bin/activate # or venv\Scripts\Activate.ps1 for PowerShell/Windows
+pip -r requirements.txt
+
+# Generate architecture diagram
+cd docs/
+python architecture.py
+```
--- docs/architecture.py
+++ docs/architecture.py
@@ -1,8 +1,36 @@
-# diagram.py
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Cluster, Diagram, Edge
+from diagrams.azure.compute import ContainerInstances, ContainerRegistries
+from diagrams.onprem.ci import GithubActions
+from diagrams.onprem.vcs import Github
+from diagrams.onprem.client import Users
+from diagrams.azure.storage import DataLakeStorage
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+with Diagram("Azure Cost Reporting", filename="architecture"):
+    devs = Users("Developers")
+
+    with Cluster("Azure"):
+        cron_job = ContainerInstances("ELT CronJob")
+        web_app = ContainerInstances("Web App")
+        storage = DataLakeStorage("Data Lake")
+        acr = ContainerRegistries("Container Registry")
+    
+    with Cluster("GitHub"):
+        git_repo = Github("Git repository")
+        ci = GithubActions("CI Pipeline")
+        cd = GithubActions("CD Pipeline")
+
+    devs >> Edge(label="Merge PRs into main branch") >> git_repo
+
+    git_repo >> Edge(label="triggers") >> ci
+
+    ci >> Edge(label="1. push image into") >> acr
+    ci >> Edge(label="2. trigger") >> cd
+
+    cd >> \
+        Edge(label="deploy") >> \
+        [web_app, cron_job] >> \
+        Edge(label="pull image from") >> \
+        acr
+    
+    cron_job >> Edge(label="read and write") >> storage
+    web_app >> Edge(label="read") >> storage
